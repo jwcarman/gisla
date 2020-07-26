6fc95c28b095e17953711bc4df8b0b16f7d5c0b5
James Carman <jcarman@callibrity.com>
2020-07-26 13:47:20 -0400

    Adding testcontainers for Axon server.

--- src/test/java/io/gisla/GislaApplicationTests.java
+++ src/test/java/io/gisla/GislaApplicationTests.java
@@ -1,14 +1,30 @@
 package io.gisla;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 class GislaApplicationTests {
 
-//
-// Other Methods
-//
+	private static final int GRPC_PORT = 8124;
+	private static final int HTTP_PORT = 8024;
+
+	@Container
+	public static final GenericContainer<?> axonServer = new GenericContainer<>("axoniq/axonserver")
+			.withExposedPorts(HTTP_PORT, GRPC_PORT)
+			.waitingFor(Wait.forHttp("/actuator/info").forPort(HTTP_PORT))
+			.withStartupTimeout(Duration.of(60L, ChronoUnit.SECONDS));
+
+	static {
+		axonServer.start();
+		System.setProperty("axon.axonserver.servers", String.format("localhost:%d", axonServer.getMappedPort(GRPC_PORT)));
+	}
 
 	@Test
 	void contextLoads() {
